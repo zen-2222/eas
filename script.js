7f8239bfb5a45b317a9bb562b32c5b95e66ce8c0
M <233329682+zen-2222@users.noreply.github.com>
2026-04-08 15:58:24 +0300

    fix bugs & add opacity function

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const btn=document.querySelector('#btn')
 const container=document.querySelector('.container')
 
 
-
 btn.addEventListener('click',(e)=>{
     const grids=parseInt(prompt('how many grids type shi'))
     container.setAttribute("style","grid-template-rows:" + "repeat("+ grids + ",1fr); " + "grid-template-columns:" + "repeat("+ grids + ",1fr);" + "min-width:90vw;" + "min-height:95vh;")
@@ -20,20 +19,21 @@ function makegrid(grids){
 
     for (let i=0;i<grids ** 2;i++){
         const grid=document.createElement("div")
+        grid.style.opacity=0
         container.appendChild(grid)
     }
 
-    container.addEventListener('mouseover',(e)=>{
-        if(e.target.classList.value != "container"){
-            e.target.setAttribute('style',"background-color:black;")
-
+    
+}
+container.addEventListener('mouseover',(e)=>{
+        if(e.target.classList.value != "container"){ 
+            e.target.style.backgroundColor="black"
+            e.target.style.opacity = (parseFloat(e.target.style.opacity) + .1)
 
         }
 
 
 
     })
-}
-
 
 
