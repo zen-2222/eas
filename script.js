683abbc51632797da2d8008a90d590e92a4d8a57
M <233329682+zen-2222@users.noreply.github.com>
2026-04-08 15:05:12 +0300

    finish functionality

--- script.js
+++ script.js
@@ -2,3 +2,38 @@ const btn=document.querySelector('#btn')
 const container=document.querySelector('.container')
 
 
+
+btn.addEventListener('click',(e)=>{
+    const grids=parseInt(prompt('how many grids type shi'))
+    container.setAttribute("style","grid-template-rows:" + "repeat("+ grids + ",1fr); " + "grid-template-columns:" + "repeat("+ grids + ",1fr);" + "min-width:90vw;" + "min-height:95vh;")
+    
+    makegrid(grids)
+
+})
+
+function makegrid(grids){
+    while(container.hasChildNodes()){
+        const child= container.firstChild
+        container.removeChild(child)
+
+    }
+
+    for (let i=0;i<grids ** 2;i++){
+        const grid=document.createElement("div")
+        container.appendChild(grid)
+    }
+
+    container.addEventListener('mouseover',(e)=>{
+        if(e.target.classList.value != "container"){
+            e.target.setAttribute('style',"background-color:black;")
+
+
+        }
+
+
+
+    })
+}
+
+
+
